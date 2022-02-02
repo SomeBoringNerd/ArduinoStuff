49765354df8e218df7c40183308412f49aaa19fb
A Boring Nerd <pro.corentynsauvage@gmail.com>
2022-02-02 10:56:41 +0100

    Create Blink.ino

---
+++ Blinking/Blink/Blink.ino
@@ -0,0 +1,21 @@
+#define LED_BUILTIN 2
+
+// s'execute au démarrage
+void setup()
+{
+    pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// s'execute en bouble
+void loop()
+{
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(FrequencyToMS(5));
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(FrequencyToMS(5));
+}
+
+int FrequencyToMS(int hz)
+{
+    return 1000 / hz;
+}
