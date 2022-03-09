9c610bfdf0884668c6d2cb8bca14ddba4007a724
A Boring Nerd <pro.corentynsauvage@gmail.com>
2022-03-09 12:06:35 +0100

    Update led_color_4.ino

--- led_color_4/led_color_4.ino
+++ led_color_4/led_color_4.ino
@@ -1,34 +1,45 @@
 #define LED_LIGHT 6
-
 #include <Adafruit_NeoPixel.h>
-
 Adafruit_NeoPixel pixels(60, LED_LIGHT, NEO_GRB + NEO_KHZ800);
 
-// s'execute au démarrage
+bool already_looping = false;
+
 void setup()
 {
   pixels.begin();
-  for (int i = 0; i < 56; i++) {
+  pixels.clear();
+  pinMode(2, INPUT);
+}
+
+void loop()
+{
+    // s'éxécute si le bouton est appuyé et que la routine ne tourne pas
+    if(digitalRead(2) && !already_looping){
+      loop_again();
+    }
+}
+
+//boucle le programe quand on appuie sur le bouton pressoir et qu'il n'est pas déjà en train de tourner
+// afin de ne pas réécrire le même code trop de fois
+void loop_again()
+{
+  already_looping = true;
+  for (int i = 0; i < 57; i++) {
     pixels.clear();
-    turn_light(i + 3, 255, 0, 0);
-    turn_light(i + 2, 0, 255, 0);
-    turn_light(i + 1, 0, 0, 255);
-    turn_light(i, 255, 255, 0);
+    turn_light(i + 3, 255, 0, 0); // rouge
+    turn_light(i + 2, 0, 255, 0); // vert
+    turn_light(i + 1, 0, 0, 255); // bleu
+    turn_light(i, 255, 255, 0);   // jaune
     pixels.show();
     delay(25);
   }
-}
-
-// s'execute en bouble
-void loop()
-{
-  
+  already_looping = false;
 }
 
 /**
-   @DESC : allume une lampe avec la couleur qu'on veut
-   @PARAM color : une des 16 couleurs 16 bits
+   @DESC : allume une lampe avec la couleur qu'on veut (wrapper plus simple d'usage)
    @PARAM light_pos : quelle LED on veut modifier ? (int non nul, ni négatif, mais normalement, seul un débile ferai ça donc pas besoin de faire de fallback)
+   @PARAM red, blue, green : couleur en RGB
 */
 void turn_light(int pos, int red, int blue, int green)
 {
